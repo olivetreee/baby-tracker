d1ddc03e733e1bf8a0b7c1453fde64cb60e42905
erico <eric.oliveira@accolade.com>
2019-03-04 04:07:45 -0800

    Update latest before putting new item

--- baby-tracker-logger/index.js
+++ baby-tracker-logger/index.js
@@ -21,35 +21,78 @@ const clickToCategory = {
 
 const feedEveryHours = 3;
 
+const ddbQueryLatest = (category) => new Promise((resolve, reject) => {
+	var params = {
+		TableName,
+		IndexName: 'category-latest-index',
+		ExpressionAttributeValues: {
+			':category': category,
+			':latest': 'true'
+		},
+		KeyConditionExpression: 'category = :category and latest = :latest'
+	};
+	dynamodb.query(params, (err, data) => {
+		if (err) {
+			reject(err);
+			return;
+		}
+		resolve(data);
+	});
+});
+
+const ddbPut = Item => new Promise((resolve, reject) => {
+	dynamodb.put({ TableName, Item }, (err, data) => {
+		if (err) {
+			reject(err);
+			return;
+		}
+		resolve(data);
+	});
+});
+
+const ddbUpdate = (orgItem) => new Promise((resolve, reject) => {
+	const params= {
+		TableName,
+		Key: { category: orgItem.category, timestamp: orgItem.timestamp },
+		ExpressionAttributeNames: {'#latest' : 'latest'},
+		ExpressionAttributeValues: {
+			':latestStatus' : 'false'
+		},
+		UpdateExpression: 'set #latest = :latestStatus'
+	};
+	dynamodb.update(params, (err, data) => {
+		if (err) {
+			reject(err);
+			return;
+		}
+		resolve(data);
+	});
+});
+
 exports.handler = async (event, context, callback) => {
 	const timestamp = new Date().getTime();
 	const { clickType } = event;
 	const category = clickToCategory[clickType];
 	const Item = {
 		category,
-		timestamp
+		timestamp,
+		latest: 'true'
 	}
 
-	const ddbPut = () => new Promise((resolve, reject) => {
-		dynamodb.put({ TableName, Item }, (err, data) => {
-			console.log('@@@inside DDB');
-			if (err) {
-				reject(err);
-				return;
-			}
-			resolve(data);
-		});
-	});
-
 	try {
-		console.log('@@@Gonna go to DDB now');
-		await ddbPut();
+		console.log('Starting DDB Query...');
+		const queriedData = await ddbQueryLatest(category);
+		const curretLatest = queriedData.Items[0];
+		console.log('Finished Query. Starting Update...');
+		await ddbUpdate(curretLatest);
+		console.log('Finished Update. Starting Put...');
+		await ddbPut(Item);
 	} catch (err) {
 		console.error('Error when saving data to Dynamo', err);
 		return err;
 	}
 
-	console.log('@@@abt to return');
+	console.log('Finished all operations');
 	return {
 		statusCode: 200
 	};
