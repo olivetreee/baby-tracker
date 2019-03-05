7a8f4dc7f5c4b90eec907c68cba8da059fa33188
erico <eric.oliveira@accolade.com>
2019-03-05 04:02:30 -0800

    Add utils for pring date/time

---
+++ baby-tracker-client/src/utils.js
@@ -0,0 +1,22 @@
+export const hoursToMs = hours => hours * 60 * 60 * 1000;
+
+export const printHoursAndMinutesFromDiff = minutes => {
+	if (minutes < 60) {
+		return `${minutes} m`;
+	}
+	const hours = Math.floor(minutes / 60);
+	const remainingMinutes = minutes % 60
+	return `${hours}:${remainingMinutes} h`;
+}
+
+export const printHoursAndMinutesFromDate = timestamp => {
+	const date = new Date(timestamp);
+	return `${date.getHours()}:${date.getMinutes()}`;
+}
+
+export const categories = {
+	feed: 'feed',
+	poop: 'poop',
+	pee: 'pee'
+};
+
