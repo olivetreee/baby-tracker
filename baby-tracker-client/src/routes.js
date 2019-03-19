4bd1d3b5a6d2adf4347d6804564a5335ed7f9187
erico <eric.oliveira@accolade.com>
2019-03-19 08:25:04 -0700

    No more need for Callback component

--- baby-tracker-client/src/routes.js
+++ baby-tracker-client/src/routes.js
@@ -2,23 +2,10 @@ import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import Login from './Auth/Login';
 import App from './App';
-import Callback from './Callback/Callback';
 import Auth from './Auth/Auth';
 import history from './history';
 
 const auth = new Auth();
-
-const handleAuthentication = ({location}) => {
-	if (/access_token|id_token|error/.test(location.hash)) {
-		auth.handleAuthentication()
-			.catch(err => {
-				if (!auth.isAuthenticated()) {
-					console.error('Auth error:', err);
-					// auth.login();
-				}
-			});
-	}
-}
 
 export const makeMainRoutes = () => {
 	return (
@@ -26,10 +13,6 @@ export const makeMainRoutes = () => {
 				<div>
 					<Route path="/" exact render={(props) => <Login auth={auth} {...props} />} />
 					<Route path="/app" render={(props) => <App auth={auth} {...props} />} />
-					<Route path="/callback" render={(props) => {
-						handleAuthentication(props);
-						return <Callback {...props} />
-					}}/>
 				</div>
 			</Router>
 	);
