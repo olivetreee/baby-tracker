8d2a9100f7b9f0e355f3d0be062e054524bd552a
erico <eric.oliveira@accolade.com>
2019-03-24 02:42:46 -0700

    Timepicker working on iOS

--- baby-tracker-client/src/TimePicker/TimePicker.jsx
+++ baby-tracker-client/src/TimePicker/TimePicker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './TimePicker.css';
 
@@ -6,29 +6,26 @@ const TimePicker = ({
 	category,
 	onSelect
 }) => {
-	const time = new Date();
-	const hhmm = `${time.getHours()}:${time.getMinutes()}`;
-
-	const handleSelected = evt => {
-		const selected = evt.target.value;
-		const now = new Date();
-		const h = selected.split(':').shift();
-		const m = selected.split(':').pop();
-		const timestamp = new Date(now.getFullYear(), now.getMonth(), now.getDate(), h, m).getTime();
-		onSelect(timestamp);
-		return timestamp;
-	}
+	const [date, setDate] = useState(new Date());
+	let hhmm = `${date.getHours()}:${date.getMinutes()}`;
 
 	// this is dumb, but apparently needed:
 	// Android: onChange fires when you click done, not every time you change the thing;
 	// iOS: onChange fires everytime you change the input. When you click set, it blurs the time input
 	// Therefore, we check which browser we're using so we can handle the event correctly.
-	const handleChange = evt => {
+	const handleInput = evt => {
 		evt.preventDefault();
-		if (navigator.appVersion.includes('iPhone')) {
+		const selected = evt.target.value;
+		const [h, m] = selected.split(':');
+		hhmm = `${h}:${m}`;
+		const now = new Date();
+		const newDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), h, m);
+		setDate(newDate);
+
+		if (navigator.appVersion.includes('iPhone') || !navigator.appVersion.includes('Chrome')) {
 			return;
 		}
-		handleSelected(evt);
+		onSelect(newDate.getTime());
 	}
 
 	const handleBlur = evt => {
@@ -36,7 +33,7 @@ const TimePicker = ({
 		if (navigator.appVersion.includes('Android')) {
 			return;
 		}
-		handleSelected(evt);
+		onSelect(date.getTime());
 	}
 
 	return (
@@ -45,7 +42,7 @@ const TimePicker = ({
 			type="time"
 			className="time-picker"
 			onBlur={ handleBlur }
-			onChange={ handleChange }
+			onInput={ handleInput }
 			value={ hhmm } />
 	);
 }
