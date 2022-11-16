dad1394045a1af905a8d6c9025d4c3e45cc03e46
Eric Oliveira <eoliveira@indeed.com>
2022-11-16 15:48:46 -0800

    main: Wait until Google auth script can be init

--- baby-tracker-client/src/Auth/Login.jsx
+++ baby-tracker-client/src/Auth/Login.jsx
@@ -5,49 +5,32 @@ import './Login.css';
 class Login extends Component {
 	componentDidMount() {
 		const { auth } = this.props;
-		console.log('@@@auth.isAuthenticated', auth.isAuthenticated());
 		if (auth.isAuthenticated()) {
 			this.props.history.replace('/app');
 			return;
 		}
+		this.initGoogleAuth();
+	}
 
-		window.google.accounts.id.initialize({
-			client_id: "1040367907541-0o6a7b5isda4f8mmbvqle9ad7mj654mf.apps.googleusercontent.com",
-			callback: (data) => auth.onSignIn(data)
-		});
-		window.google.accounts.id.prompt();
+	initGoogleAuth() {
+		const { auth } = this.props;
+		setTimeout(() => {
+			if (window.google && window.google.accounts) {
+				window.google.accounts.id.initialize({
+					client_id: "1040367907541-0o6a7b5isda4f8mmbvqle9ad7mj654mf.apps.googleusercontent.com",
+					callback: (data) => auth.onSignIn(data)
+				});
+				window.google.accounts.id.prompt();
+				return;
+			}
+			console.log("Google auth script not ready yet. Trying again in a bit...");
+			this.initGoogleAuth();
+		}, 250)
 	}
 
 	render() {
-		const { auth } = this.props;
-		const onSuccess = bla => console.log('@@@bla', bla);
-
 		return (
-			<div className="login">
-				{/* <div id="g_id_onload"
-						data-client_id="1040367907541-0o6a7b5isda4f8mmbvqle9ad7mj654mf.apps.googleusercontent.com"
-						data-context="signin"
-						data-callback={onSuccess}
-						data-itp_support="true">
-				</div> */}
-				{/* <div id="g_id_onload"
-					data-client_id="1040367907541-0o6a7b5isda4f8mmbvqle9ad7mj654mf.apps.googleusercontent.com"
-					data-context="signin"
-					data-ux_mode="popup"
-					data-callback={ (bla) => {console.log("@@@bla", bla)} }
-					data-nonce=""
-					data-auto_prompt="false">
-				</div>
-
-				<div className="g_id_signin"
-					data-type="standard"
-					data-shape="pill"
-					data-theme="outline"
-					data-text="signin_with"
-					data-size="large"
-					data-logo_alignment="left">
-				</div> */}
-			</div>
+			<div className="login" />
 		);
 	}
 }
